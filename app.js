5c52fdb1e2920d060b44fc4afa029308754d7af2
Mikko Sillanpaa <mikkooskarviktor@gmail.com>
2020-07-28 11:31:28 +0200

    Finished adding arrow nav, and other stuff

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
-$(document).ready(function() {
-  $(".slider-carousel").slick({
-	infinite: !0,
-	slidesToShow: 4,
-	slidesToScroll: 1,
-	autoplay: !1,
-	arrows:true,
-	prevArrow: $(".testimonial-carousel-controls .prev"),
-	nextArrow: $(".testimonial-carousel-controls .next"),
-	responsive: [{
-	  breakpoint: 1200,
-	  settings: {
-	    slidesToShow: 3
-	  }
-	}, {
-	  breakpoint: 992,
-	  settings: {
-	    slidesToShow: 2
-	  }
-	}, {
-	  breakpoint: 600,
-	  settings: {
-	    slidesToShow: 1
-	  }
-	}]
-  });
-});
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "flex";
+}
+
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 37:
+            //left
+            e.preventDefault();
+            slideIndex--;
+            showSlides(slideIndex);
+            break;
+        case 39:
+            //right
+            e.preventDefault();
+            slideIndex++;
+            showSlides(slideIndex);
+            break;
+    }
+}
